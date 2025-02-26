c05829a113dd01b9cb7f34d4aa71c6c4921460f1
Hubert Lin <linhub15@users.noreply.github.com>
2025-02-26 10:20:16 -0700

    feat: add ghungroo-3 event page

--- src/_components/icons/menu.tsx
+++ src/_components/icons/menu.tsx
@@ -6,9 +6,9 @@ export default () => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     className="icon icon-tabler icons-tabler-outline icon-tabler-menu-4"
   >
     <title>Menu</title>
--- src/_components/icons/x.tsx
+++ src/_components/icons/x.tsx
@@ -6,11 +6,12 @@ export default () => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     className="icon icon-tabler icons-tabler-outline icon-tabler-x"
   >
+    <title>Close</title>
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M18 6l-12 12" />
     <path d="M6 6l12 12" />
